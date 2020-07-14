7d0d7567036623f8626df5f17c80d1a5db1f7adc
Brian Kuan <bigmic15@gmail.com>
2020-07-14 13:57:51 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.css';
+import Movies from './components/movies';
+
+export default function App() {
+  return (
+    <main className="container">
+      <Movies />
+    </main>
+  );
+}
