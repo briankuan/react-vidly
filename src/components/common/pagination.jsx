2b85e221cc5bada08d5ea1d1579aa801f20ab59d
Brian Kuan <26177329+briankuan@users.noreply.github.com>
2020-07-16 21:00:41 +1000

    Displaying Pages

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import _ from 'lodash';
 
 const Pagination = props => {
-  return null;
+  const { pageSize, itemsCount } = props;
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+
+  if (pagesCount === 1) return null;
+
+  const pages = _.range(1, pagesCount + 1);
+  return (
+    <ul className="pagination">
+      {pages.map(page => (
+        <li key={page} className="page-item">
+          <a className="page-link" href="http://#">
+            {page}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 };
+
 export default Pagination;
