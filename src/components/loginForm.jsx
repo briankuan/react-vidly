0019a8e2d624a58276a9095585f41228ee81d33f
Brian Kuan <26177329+briankuan@users.noreply.github.com>
2020-07-22 11:36:31 +1000

    Handling Form Submission

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log('submitted');
+  };
+
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input id="username" type="text" className="form-control" />
@@ -14,7 +20,7 @@ class LoginForm extends Component {
             <label htmlFor="password">Password</label>
             <input id="password" type="text" className="form-control" />
           </div>
-          <button className="btn btn=primary">Login</button>
+          <button className="btn btn-primary">Login</button>
         </form>
       </div>
     );
