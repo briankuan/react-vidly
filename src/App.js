d7c5b15695eff9b9ffa255f89794544ca3532896
Brian Kuan <26177329+briankuan@users.noreply.github.com>
2020-07-22 11:31:18 +1000

    Building a Bootstrap Form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MovieForm from './components/movieForm';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
+import LoginForm from './components/loginForm';
 import './app.css';
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
