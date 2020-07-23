7a2077e93e778a2d06caaabd0030bb39d3c1c7e4
Brian Kuan <26177329+briankuan@users.noreply.github.com>
2020-07-23 13:44:51 +1000

    Displaying Validation Errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -14,7 +14,7 @@ class LoginForm extends Component {
       errors.username = 'Username is required.';
     if (account.password.trim() === '')
       errors.password = 'Password is required.';
-    return Object.keys(errors) === 0 ? null : errors;
+    return Object.keys(errors) === 0 ? {} : errors;
   };
 
   handleSubmit = e => {
@@ -35,7 +35,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -45,12 +45,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
